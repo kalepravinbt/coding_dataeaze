ef37d651f01e8649375f82dab3dc24b25eea940d
kalepravinbt <81917713+kalepravinbt@users.noreply.github.com>
2021-04-04 21:05:27 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ' ---------------------------Question 1----------------------------------------'
+spark-submit  /home/pravin/Desktop/Cdac_Data/company_coding/Dataeaze_coding_test/Dataeaze_spark+java/Spark1_5/que1.py
+echo '--------------------------- Question 2----------------------------------------'
+spark-submit  /home/pravin/Desktop/Cdac_Data/company_coding/Dataeaze_coding_test/Dataeaze_spark+java/Spark1_5/que2.py
+echo '--------------------------- Question 4-----------------------------------------'
+spark-submit  /home/pravin/Desktop/Cdac_Data/company_coding/Dataeaze_coding_test/Dataeaze_spark+java/Spark1_5/que4.py
+echo ' ----------------------------Question 5------------------------------------------'
+spark-submit  /home/pravin/Desktop/Cdac_Data/company_coding/Dataeaze_coding_test/Dataeaze_spark+java/Spark1_5/que5.py
+echo ' ----------------------------Bonus Quastion-------------------------------------'
+spark-submit  /home/pravin/Desktop/Cdac_Data/company_coding/Dataeaze_coding_test/Dataeaze_spark+java/Spark1_5/bonus.py
+echo '---------------------------------end script-------------------------------------------'
